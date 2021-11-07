a097ffb1054c96e2dee7ae89601e83eb6e2fbe2e
aushev06 <34713423+aushev06@users.noreply.github.com>
2021-11-07 22:47:27 +0300

    Update HomeService.php

--- app/Blog/Services/HomeService.php
+++ app/Blog/Services/HomeService.php
@@ -26,64 +26,69 @@ class HomeService
 
     private function getPosts(Request $request): array
     {
-      $query = Post::query()->where('status', StatusEnum::STATUS_ACTIVE);
+        $query = Post::query()->where('status', StatusEnum::STATUS_ACTIVE);
 
-      $query->when($request->new, function (Builder $builder) {
-        return $builder->orderByDesc('id');
-      }, function (Builder $builder) {
-        $builder->orderByDesc('likes_count');
-        $builder->orderByDesc('dislikes_count');
-        $builder->orderByDesc('views');
-        $builder->orderByDesc('comments_count');
-        return $builder;
-      });
+        $query->when($request->new, function (Builder $builder) {
+            return $builder->orderByDesc('id');
+        }, function (Builder $builder) {
+            $builder->orderByDesc('likes_count');
+            $builder->orderByDesc('dislikes_count');
+            $builder->orderByDesc('views');
+            $builder->orderByDesc('comments_count');
+            return $builder;
+        });
 
-      $query->select([
-        'id',
-        'title',
-        'slug',
-        'img',
-        'description',
-        'views',
-        'likes',
-        'dislikes',
-        'created_at',
-      ]);
+        $query->select([
+            'id',
+            'title',
+            'slug',
+            'img',
+            'description',
+            'views',
+            'likes',
+            'dislikes',
+            'created_at',
+        ])
+            ->with(['user'])
+            ->withCount(['likes', 'dislikes', 'comments']);
 
-      $query->when($request->user('api'), function (Builder $builder, User $user) {
-          $builder->addSelect(['liked_type' => function (QB $qb) use ($user) {
-              return $qb->selectRaw(Likeable::getUserLikedTypeQuery('posts', 'Post', $user));
-          }]);
-      });
+        $query->when($request->user('api'), function (Builder $builder, User $user) {
+            $builder->addSelect(['liked_type' => function (QB $qb) use ($user) {
+                return $qb->selectRaw(Likeable::getUserLikedTypeQuery('posts', 'Post', $user));
+            }]);
+        });
 
-      return $query->get()->toArray();
+        return $query->get()->toArray();
     }
 
-    private function getComments() {
-      return Comment::with('post:id,slug,title')
-        ->take(10)
-        ->orderByDesc('id')
-        ->get()
-        ->toArray();
+    private function getComments()
+    {
+        return Comment::with('post:id,slug,title')
+            ->take(10)
+            ->orderByDesc('id')
+            ->get()
+            ->toArray();
     }
 
-    private function getCategories() {
-      return Category::take(10)
-        ->orderByDesc('name')
-        ->get()
-        ->toArray();
+    private function getCategories()
+    {
+        return Category::take(10)
+            ->orderByDesc('name')
+            ->get()
+            ->toArray();
     }
 
-    public function getMainInfo(Request $request) {
-      $posts = $this->getPosts($request);
-      $comments = $this->getComments();
-      $categories = $this->getCategories();
+    public function getMainInfo(Request $request)
+    {
+        $posts = $this->getPosts($request);
+        $comments = $this->getComments();
+        $categories = $this->getCategories();
 
-      return json_encode([
-        'posts' => $posts,
-        'comments' => $comments,
-        'categories' => $categories
-      ]);
+        return json_encode([
+            'posts' => $posts,
+            'comments' => $comments,
+            'categories' => $categories
+        ]);
     }
 
 }
