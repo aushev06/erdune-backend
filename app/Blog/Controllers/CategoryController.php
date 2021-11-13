b36b6118a00ec2cda47b80e9ed9cdfc742d3ce6a
Archakov Dennis <amonbower@gmail.com>
2021-11-14 02:21:57 +0300

    fix

--- app/Blog/Controllers/CategoryController.php
+++ app/Blog/Controllers/CategoryController.php
@@ -16,7 +16,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return Category::query()->get();
+        return Category::query()->width(['posts'])->get();
     }
 
     /**
